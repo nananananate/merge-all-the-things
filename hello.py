a69532e92a95c4651a366e80a65402971a5a69a5
Nathaniel Louis Tisuela <natetisuela@gmail.com>
2020-10-02 15:31:57 -0700

    hello world

---
+++ hello.py
@@ -0,0 +1,16 @@
+# Made by nananananate
+# Check out CourseCake!
+
+
+def hello_world():
+    print("hello world")
+
+
+
+
+def main():
+    hello_world()
+
+
+if __name__ == '__main__':
+    main()
